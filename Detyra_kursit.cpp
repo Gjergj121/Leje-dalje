853bdd4c99c37514391cc9b00dbc43d90cf9eb15
Gjergj Plepi <gjergjplepi12@gmail.com>
2020-05-22 00:53:01 +0200

    Konfigurime

--- Detyra_kursit.cpp
+++ Detyra_kursit.cpp
@@ -8,11 +8,14 @@ struct data{
     char mbiemer[30];
     int ditelindja;
     int id_familja;
-}*dataptr, shtetas;
+}*dataptr;
 /* prototipet e funksioneve */
 int menu();
+void perzgjedhja(int counter, int size);
 int shto_shtetas(int count, int madhesia);
+int shto_shtetas(int count, int madhesia, struct data shtetas);
 void ndrysho_shtetas(int count);
+void ndrysho_shtetas(int count, struct data shtetas);
 
 int main(){
     FILE *fptr; // krijojme nje pointer qe pointon ne nje file
@@ -20,7 +23,7 @@ int main(){
         printf("Gabim ne hapjen e file-it");
         return 0;
     }
-    int counter = 0, size = 1000;
+    int counter = 0, size = 3;
     dataptr = (struct data*) malloc(size*sizeof(struct data)); // alokoj memorjen
 
     /*hedhim elementet e file ne pointerin dataptr */
@@ -42,6 +45,29 @@ int main(){
     }
     printf("\nKemi %d qytetare ", counter);
 */
+    perzgjedhja(counter, size);
+
+    return 0;
+}
+
+int menu(){
+    int choice;
+    printf("\n-----------------------------------------");
+    printf("\n              Detyre  Kursi\n");
+    printf("\n1 - Shto nje shtetas te ri");
+    printf("\n2 - Ndrysho nje shtetas");
+    printf("\n3 - Apliko per Leje Dalje");
+    printf("\n4 - Kontrollo nese nje shtetas ka leje");
+    printf("\n5 - Printo leje daljet");
+    printf("\n6 - Printo listen e 10 shtetasve\n    qe kane levizur me shpesh dhe\n    ato 10 qe kane levizur me pak");
+    printf("\n0 - Mbyll programin");
+    printf("\n-----------------------------------------\n");
+    printf("\nZgjedhja juaj : ");
+    scanf("%d", &choice);
+    return choice;
+}
+
+void perzgjedhja(int counter, int size){
     int zgjedhja = menu();
 
     while(zgjedhja != 0){
@@ -75,27 +101,11 @@ int main(){
         printf("\nZgjedhja juaj : ");
         scanf("%d", &zgjedhja);
     }
-    return 0;
-}
-
-int menu(){
-    int choice;
-    printf("\n-----------------------------------------");
-    printf("\n              Detyre  Kursi\n");
-    printf("\n1 - Shto nje shtetas te ri");
-    printf("\n2 - Ndrysho nje shtetas");
-    printf("\n3 - Apliko per Leje Dalje");
-    printf("\n4 - Kontrollo nese nje shtetas ka leje");
-    printf("\n5 - Printo leje daljet");
-    printf("\n6 - Printo listen e 10 shtetasve\n    qe kane levizur me shpesh dhe\n    ato 10 qe kane levizur me pak");
-    printf("\n0 - Mbyll programin");
-    printf("\n-----------------------------------------\n");
-    printf("\nZgjedhja juaj : ");
-    scanf("%d", &choice);
-    return choice;
+    return;
 }
 
 int shto_shtetas(int count, int madhesia){
+    struct data shtetas;
     printf("Vendosni IDNR : ");
     scanf("%s", shtetas.idnr);
     printf("Vendosni emrin : ");
@@ -114,7 +124,14 @@ int shto_shtetas(int count, int madhesia){
         }
     }
     if(i == count){ // tregon qe shtetasi eshte unik
-        for(j = count-1; j >= 0; j--){
+        count = shto_shtetas(count, madhesia, shtetas);
+    }
+    return count;   
+}
+
+int shto_shtetas(int count, int madhesia, struct data shtetas){
+    int j, k;
+    for(j = count-1; j >= 0; j--){
             if(shtetas.id_familja == (dataptr+j)->id_familja ){
                 for(k = count-1; k > j; k--){
                     *(dataptr+k+1) = *(dataptr+k);
@@ -128,11 +145,11 @@ int shto_shtetas(int count, int madhesia){
         }
         count++;
         dataptr = (struct data*) realloc(dataptr, madhesia*sizeof(struct data)+1 );
-    }
-    return count;   
+        return count;
 }
 
 void ndrysho_shtetas(int count){
+    struct data shtetas;
     printf("Vendosni IDNR e shtetasit : ");
     scanf("%s", shtetas.idnr);
     printf("Ndryshoni te dhenat :\n");
@@ -144,6 +161,12 @@ void ndrysho_shtetas(int count){
     scanf("%d", &shtetas.ditelindja);
     printf("Vendosni ID e familjes : ");
     scanf("%d", &shtetas.id_familja);
+
+    ndrysho_shtetas(count, shtetas);
+    return;
+}
+
+void ndrysho_shtetas(int count, struct data shtetas){
     int i, j, k;
     for(i = 0; i < count; i++){
         if(strcmp(shtetas.idnr, (dataptr+i)->idnr) == 0){
@@ -151,7 +174,7 @@ void ndrysho_shtetas(int count){
             strcpy((dataptr+i)->mbiemer, shtetas.mbiemer);
             (dataptr+i)->ditelindja = shtetas.ditelindja;
             (dataptr+i)->id_familja = shtetas.id_familja;
-            /* e vendos ne vendin e duhur ne liste */
+            /* e vendos ne vendin e duhur ne liste (Bubble sort) */
             struct data temp;
             for(j = 0; j < count-1; j++){
                 for(k = 0; k < count-1-j; k++){
@@ -165,6 +188,6 @@ void ndrysho_shtetas(int count){
             return;
         }
     } 
-    printf("Ky shtetas nuk ndodhet ne sistem");
+    printf("Ky shtetas nuk ndodhet ne sistem\n");
     return;
 }
